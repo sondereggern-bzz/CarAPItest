083b9acfa5e91b5b2fe5606fd0f806459aeab9ac
sondereggern@bzz.ch <sondereggern@bzz.ch>
2024-04-18 16:20:32 +0200

    v1.02 -> put Method implementiert

--- app.js
+++ app.js
@@ -20,6 +20,19 @@ app.post('/api/automodelle', (req, res) => {
     res.status(201).json(neuAuto);
 });
 
+app.put('/api/automodelle/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const { marke, modell, baujahr, motor, leistung } = req.body;
+    const index = datenbank.automodelle.findIndex(auto => auto.id === id);
+    if (index !== -1) {
+        datenbank.automodelle[index] = { id, marke, modell, baujahr, motor, leistung };
+        res.json(datenbank.automodelle[index]);
+    } else {
+        res.status(404).send('Auto nicht gefunden');
+    }
+});
+
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
