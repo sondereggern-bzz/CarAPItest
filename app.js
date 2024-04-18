e1f646d96aec5f1e1d72ce1bce7ff51466cd7677
sondereggern@bzz.ch <sondereggern@bzz.ch>
2024-04-18 16:31:33 +0200

    v1.03 -> delete Method Implementiert

--- app.js
+++ app.js
@@ -32,6 +32,17 @@ app.put('/api/automodelle/:id', (req, res) => {
     }
 });
 
+app.delete('/api/automodelle/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = datenbank.automodelle.findIndex(auto => auto.id === id);
+    if (index !== -1) {
+        datenbank.automodelle.splice(index, 1);
+        res.status(204).send();
+    } else {
+        res.status(404).send('Auto nicht gefunden');
+    }
+});
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
